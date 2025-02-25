ab9d3e9b9043cbba039d1a233ebd083da1bc995a
JaredMeeks <jared13meeks56@gmail.com>
2025-02-25 14:39:03 -0500

    finished the exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0) return undefined;
+  if (num === 0) return 1;
+  let x = num - 1;
+  while (x > 1) {
+    num *= x;
+    x--;
+  }
+  return num;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+    if(!death) { 
+        death = new Date().getFullYear();
+        // if there is no death year, meaning the person is still alive, get the current year to use in its place.
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(array) {
+    return array.reduce((a, b) => {
+        const age = getAge(a.yearOfBirth, a.yearOfDeath);
+        const nextAge = getAge(b.yearOfBirth, b.yearOfDeath);
+        return age < nextAge ? b : a;
+    })
 };
 
 // Do not edit below this line
